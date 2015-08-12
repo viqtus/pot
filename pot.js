4dc6c59274506edc70de3e0f8d6169bf17ff7007
viqtus <viqtus@gmail.com>
2015-08-12 22:13:19 +0400

    0.2

---
+++ pot.js
@@ -0,0 +1,292 @@
+var window = window;
+
+var canvas = window.document.createElement('canvas');
+	canvas.resize = function(resize)
+	{
+		if((game.event.window.resize) || (resize))
+		{
+			canvas.height = window.innerHeight;
+			canvas.width = window.innerWidth;
+		};
+	};
+var context = canvas.getContext('2d');
+
+var game =
+{
+	animate: function(animation, x, y, interval)
+	{
+		var animate =
+		{
+			frame: 0,
+			interval: interval,
+			type: 'animate',
+			x: x,
+			y: y
+		};
+
+		if(animation.time <= (animation.images.length - 1) * animate.interval)
+		{
+			animate.frame = Math.round(animation.time / animate.interval);
+		}
+		else
+		{
+			animation.time = 0;
+		};
+
+		if(game.event.tick)
+		{
+			animation.time += game.options.interval;
+		};
+		animate.image = animation.images[animate.frame];
+
+		game.scene.push(animate);
+	},
+
+	animations:
+	{
+		load: function(id, images)
+		{
+			var animation =
+			{
+				time: 0
+			};
+			animation.images = [];
+			for(var i = 0; i < images.length; i++)
+			{
+				animation.images.push(game.images[images[i]]);
+			};
+			game.animations[id] = animation;
+		}
+	},
+
+	data:
+	{
+		event:
+		{
+			mouse:
+			{
+				down:
+				{
+					x: undefined,
+					y: undefined
+				},
+				up:
+				{
+					x: undefined,
+					y: undefined
+				}
+			}
+		}
+	},
+
+	draw: function()
+	{
+		context.beginPath();
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		for(var i = 0; i < game.scene.length; i++)
+		{
+			switch(game.scene[i].type)
+			{
+				case 'animate':
+					context.drawImage(game.scene[i].image, game.scene[i].x, game.scene[i].y, game.scene[i].image.width, game.scene[i].image.height);
+					break;
+				case 'image':
+					context.drawImage(game.scene[i].image, game.scene[i].x, game.scene[i].y, game.scene[i].image.width, game.scene[i].image.height);
+					break;
+				case 'text':
+					context.fillStyle = game.scene[i].color;
+					context.font = game.scene[i].size + 'px ' + game.scene[i].family;
+					context.textAlign = game.scene[i].align;
+					context.fillText(game.scene[i].text, game.scene[i].x, game.scene[i].y);
+					break;
+			};
+		};
+		game.scene = [];
+		context.closePath();
+	},
+
+	event:
+	{
+		mouse:
+		{
+			down: false,
+			up: false
+		},
+		tick: false,
+		window:
+		{
+			load: false,
+			resize: false
+		}
+	},
+
+	images:
+	{
+		load: function(id, src)
+		{
+			var image = new Image();
+				image.src = src;
+			game.images[id] = image;
+		}
+	},
+
+	loading: function()
+	{
+		canvas.resize(true);
+		game.set.icon(game.images.pot);
+	},
+
+	mode: 'play',
+
+	options:
+	{
+		font:
+		{
+			align: 'left',
+			color: 'black',
+			family: 'monospace',
+			size: 12
+		},
+		interval: 80,
+		volume: 1
+	},
+
+	paint: function(image, x, y)
+	{
+		var paint =
+		{
+			image: image,
+			type: 'image',
+			x: x,
+			y: y
+		};
+		game.scene.push(paint);
+	},
+
+	play: function(sound, volume, loop)
+	{
+		sound.loop = (loop) ? true : false;
+		sound.volume = (volume) ? volume * game.options.volume : game.options.volume;
+		sound.play();
+	},
+
+	preloading: function()
+	{
+		window.document.body.appendChild(canvas);
+		canvas.resize(true);
+	},
+
+	print: function(text, x, y, align, size, color, family)
+	{
+		var print =
+		{
+			text: text,
+			type: 'text',
+			x: x,
+			y: y
+		};
+		print.align = (align) ? align : game.options.font.align;
+		print.family = (family) ? family : game.options.font.family;
+		print.color = (color) ? color : game.options.font.color;
+		print.size = (size) ? size : game.options.font.size;
+		game.scene.push(print);
+	},
+
+	scene: [],
+
+	set:
+	{
+		icon: function(image)
+		{
+			var icon = window.document.getElementById('ICON');
+
+			if(icon)
+			{
+				window.document.head.removeChild(icon);
+			};
+
+			icon = window.document.createElement('link');
+			icon.href = image.src;
+			icon.id = 'ICON';
+			icon.rel = 'icon';
+			icon.type = 'image/png';
+
+			window.document.head.appendChild(icon);
+		}
+	},
+
+	sounds:
+	{
+		load: function(id, src)
+		{
+			var audio = new Audio(src);
+				audio.src = src;
+			game.sounds[id] = audio;
+		}
+	},
+
+	tick: undefined,
+
+	update: function()
+	{
+		canvas.resize();
+		game.paint(game.images.pot, 100, 100);
+		game.print('Hello', 100, 132, 'left', 16, '#638');
+		//game.play(game.sounds.click);
+
+		game.animate(game.animations.coin, 200, 200, 80);
+
+		game.print('300', 200, 212, 'right', 14, 'orange');
+	}
+};
+
+window.onload = function()
+{
+	game.event.window.load = true;
+	game.loading();
+	game.update();
+	game.draw();
+	game.event.window.load = false;
+};
+
+window.onmousedown = function()
+{
+	game.data.event.mouse.down.x = event.x;
+	game.data.event.mouse.down.y = event.y;
+	game.event.mouse.down = true;
+	game.update();
+	game.draw();
+	game.event.mouse.down = false;
+};
+
+window.onmouseup = function()
+{
+	game.data.event.mouse.up.x = event.x;
+	game.data.event.mouse.up.y = event.y;
+	game.event.mouse.up = true;
+	game.update();
+	game.draw();
+	game.event.mouse.up = false;
+};
+
+window.onresize = function()
+{
+	game.event.window.resize = true;
+	game.update();
+	game.draw();
+	game.event.window.resize = false;
+};
+
+game.preloading();
+
+game.tick = window.setInterval
+(
+	function()
+	{
+		game.event.tick = true;
+		game.update();
+		game.draw();
+		game.event.tick = false;
+	},
+	game.options.interval
+);
